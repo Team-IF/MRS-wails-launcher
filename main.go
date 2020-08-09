d9e99024749545ef885c7694faffa16df8efe992
Coder-Iro <Coder-Iro@teamif.io>
2020-08-09 19:04:55 +0900

    Explicit ignore errors from app.Run()

--- main.go
+++ main.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-  "github.com/leaanthony/mewn"
-  "github.com/wailsapp/wails"
+	"github.com/leaanthony/mewn"
+	"github.com/wailsapp/wails"
 )
 
 func basic() string {
-  return "World!"
+	return "World!"
 }
 
 func main() {
 
-  js := mewn.String("./frontend/build/static/js/main.js")
-  css := mewn.String("./frontend/build/static/css/main.css")
+	js := mewn.String("./frontend/build/static/js/main.js")
+	css := mewn.String("./frontend/build/static/css/main.css")
 
-  app := wails.CreateApp(&wails.AppConfig{
-    Width:  1024,
-    Height: 768,
-    Title:  "MRS Launcher",
-    JS:     js,
-    CSS:    css,
-    Colour: "#131313",
-  })
-  app.Bind(basic)
-  app.Run()
+	app := wails.CreateApp(&wails.AppConfig{
+		Width:  1024,
+		Height: 768,
+		Title:  "MRS Launcher",
+		JS:     js,
+		CSS:    css,
+		Colour: "#131313",
+	})
+	app.Bind(basic)
+	_ = app.Run()
 }
